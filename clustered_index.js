9fb13552778482f20bd33abf36163535be106ecf
Lucas Vieira <lucas@devplace.com.br>
2021-03-19 22:47:32 -0300

    clustered index

---
+++ clustered_index.js
@@ -0,0 +1,23 @@
+/* eslint-disable node/no-unpublished-require */
+'use strict';
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', worker => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  require('./build/src/index.js');
+
+  console.log(`Worker ${process.pid} started`);
+}
