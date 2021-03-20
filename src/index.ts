ad3c90d77b522c4d3676544e98b0e418db7ae339
Lucas Vieira <lucas@devplace.com.br>
2021-03-19 23:14:51 -0300

    sleep

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,31 @@ async function consumer() {
   try {
     const conn = await amqp.connect(QUEUE_URL);
     const ch = await conn.createChannel();
-    ch.consume(q, msg => {
-      if (msg !== null) {
-        console.log(
-          `Message received (PID ${process.pid}): ${msg.content.toString()}`
-        );
-        ch.ack(msg);
+    ch.consume(
+      q,
+      async msg => {
+        if (msg !== null) {
+          console.log(
+            `Message received (PID ${process.pid}): ${msg.content.toString()}`
+          );
+          await sleep(2e3);
+          ch.ack(msg);
+        }
+      },
+      {
+        // automatic acknowledgment mode,
+        // see https://www.rabbitmq.com/confirms.html for details
+        noAck: true,
       }
-    });
+    );
   } catch (error) {
     console.warn(error);
   }
 }
 consumer();
+
+async function sleep(ms: number) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
